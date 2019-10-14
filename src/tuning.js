7223856adc87b99bb94f7d327b7ee75e7c39cd0c
Adam Jagosz <adamjagosz@gmail.com>
2019-10-14 23:02:16 +0200

    add flexibility

--- src/tuning.js
+++ src/tuning.js
@@ -1,6 +1,18 @@
 const getOctave = require('./semitones').getOctave;
 const getNoteAbove = require('./semitones').getNoteAbove;
 
+function capitalize(s) {
+  return s.slice(0, 1).toUpperCase() + s.slice(1);
+}
+
+/**
+ * Deduces tuning from a string.
+ * @param {string|string[]} arg Array or hyphen-delimited list of notes of the tuning from the lowest (thickest string).
+ *                              Can include octave number after the note.
+ *                              E.g. `'E-A-D-G-B-e'`, `'E2-A2-D3-G3-B3-E4'` or `['D','A','D','F#','A','D']`.
+ *                              By default, starts from octave `2`.
+ * @returns {string[]} Array of note pitches (with octave numbers) comprising the tuning, e.g. `['D2','A2','D3','F#3','A3','D4']`.
+ */
 function parseTuning(arg) {
   if (typeof arg === 'string') {
     arg = arg.split('-');
@@ -10,8 +22,12 @@ function parseTuning(arg) {
   let octave = getOctave(lastNote);
   if (octave == null) lastNote = lastNote + 2;
   const tuning = arg.map((note) => {
-    lastNote = getNoteAbove(note, lastNote);
-    return lastNote;
+    note = capitalize(note);
+    let octave = getOctave(note);
+    if (octave == null)
+      note = getNoteAbove(note, lastNote);
+    lastNote = note;
+    return note;
   });
   return tuning;
 }
--- src/tuning.test.js
+++ src/tuning.test.js
@@ -5,4 +5,16 @@ describe('parseTuning', () => {
     const tuning = parseTuning('E-A-D-G-B-e');
     expect(tuning).toEqual(['E2', 'A2', 'D3', 'G3', 'B3', 'E4']);
   });
+  it('parses tuning with octaves', () => {
+    const tuning = parseTuning('D1-A2-D3-G4-B3-e5');
+    expect(tuning).toEqual(['D1', 'A2', 'D3', 'G4', 'B3', 'E5']);
+  });
+  it('parses array tuning', () => {
+    const tuning = parseTuning(['D','A','D','F#','A','D']);
+    expect(tuning).toEqual(['D2','A2','D3','F#3','A3','D4']);
+  });
+  it('parses array tuning with octaves', () => {
+    const tuning = parseTuning(['D2','A2','D3','G3','C4','E4']);
+    expect(tuning).toEqual(['D2','A2','D3','G3','C4','E4']);
+  });
 });
